88fad79b8edf5cdd8ac979b4661cdb4c738e9e83
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2015-01-26 14:59:21 -0500

    Update CRP.py

--- CRP.py
+++ CRP.py
@@ -9,6 +9,7 @@ Created on Thu Jan 22 15:13:06 2015
 # B Bassetti, M Zarei, M C Lagomarsino, G Bianconi, Physical Review E 80, 066118 (2009)
 
 import numpy as np
+from scipy.optimize import curve_fit
 
 def probsTableCRP(karray, alpha, theta):
     T = np.sum(karray)
@@ -19,4 +20,34 @@ def probsTableCRP(karray, alpha, theta):
 
 def sampleTableCRP(karray, alpha, theta):
     probs = probsTableCRP(karray, alpha, theta)
-    return np.random.choice(len(probs), 1, p=probs)
+    return np.int(np.random.choice(len(probs), 1, p=probs))
+    
+def zipfexpr(x, f0, alpha):
+    return f0*x**(-alpha)
+    
+class ChineseRestaurantProcess:
+    def __init__(self, alpha=0.75, theta=0.5):
+        self.reset(alpha, theta)
+        
+    def reset(self, alpha=0.75, theta=0.5):
+        self.alpha = alpha
+        self.theta = theta
+        self.karray = []
+        
+    def proceed(self, numtimes=1):
+        for i in range(numtimes):
+            tablenum = sampleTableCRP(self.karray, self.alpha, self.theta)
+            if tablenum < len(self.karray):
+                self.karray[tablenum] += 1
+            else:
+                self.karray = np.append(self.karray, 1)
+            yield self.karray
+            
+    def fit_powerlaw(self):
+        freq, bins = np.histogram(self.karray, 
+                                  bins=range(int(np.min(self.karray)), 
+                                             int(np.max(self.karray))))
+        ndim = min(len(bins), len(freq))
+        result = curve_fit(zipfexpr, bins[:ndim], freq[:ndim], 
+                           p0=[freq[0], 1.0])
+        return result
